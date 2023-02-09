9974a9eafa79e578e17482bfca7d463158b9298e
Emilio Ortiz <ortizemilio.pers@gmail.com>
2023-02-09 16:17:31 -0600

    bug fix

--- airbnb-experience-clone/src/components/Experiences.jsx
+++ airbnb-experience-clone/src/components/Experiences.jsx
@@ -8,9 +8,9 @@ const Experiences = () => {
 //'card--container' && 'text-card--container'
   return (
     <div className='exp--containers'>
-      <Card myID='firstCard' imgCard='../assets/card1.png' rate='5.0' num=' (6)' country='USA' title='Life lessons with Katie Zaferes' cost='$136' />
-      <Card imgCard='../assets/card2.png' rate='5.0' num=' (30)' country='USA' title='Learn edding photography' cost='$125' />
-      <Card myID='lastCard' imgCard='../assets/card3.png' rate='4.8' num=' (2)' country='USA' title='Group Mountain Biking' cost='$50' />
+      <Card myID='firstCard' imgCard='./src/assets/card1.png' rate='5.0' num=' (6)' country='USA' title='Life lessons with Katie Zaferes' cost='$136' />
+      <Card imgCard='./src/assets/card2.png' rate='5.0' num=' (30)' country='USA' title='Learn edding photography' cost='$125' />
+      <Card myID='lastCard' imgCard='./src/assets/card3.png' rate='4.8' num=' (2)' country='USA' title='Group Mountain Biking' cost='$50' />
     </div>
   )
 }
