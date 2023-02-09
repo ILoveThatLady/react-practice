8baa8c99bb4f8956040952add5ed1ae7ccd69a39
Emilio Ortiz <ortizemilio.pers@gmail.com>
2023-02-09 15:18:43 -0600

    End of the card area

--- airbnb-experience-clone/src/components/Card.jsx
+++ airbnb-experience-clone/src/components/Card.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 const Card = (props) => {
 
   return (
-    <div className='card--container'>
+    <div id={props.myID} className='card--container'>
         <img src={props.imgCard} />
       <div className='text-card--container'>
-        <img src=''/>
+        <img src='./src/assets/Star.png'/>
         <span>{props.rate}</span>
-        <span>{props.num} • </span>
-        <span>{props.country}</span>
-        <p>{props.title}</p>
-        <p><strong>From {props.cost}</strong> / person</p>
+        <span className='text-ligth'>{props.num} •</span>
+        <span className='text-ligth'>{props.country}</span>
+        <p className='text-title'>{props.title}</p>
+        <p className='text-price'><strong>From {props.cost}</strong> / person</p>
       </div>
     </div>
   )
--- airbnb-experience-clone/src/components/Experiences.jsx
+++ airbnb-experience-clone/src/components/Experiences.jsx
@@ -5,9 +5,12 @@ import './Experiences.css';
 const Experiences = () => {
 
 //imgCard, rate, num, country, title, cost
+//'card--container' && 'text-card--container'
   return (
     <div className='exp--containers'>
-      <Card imgCard='' rate='5.0' num=' (6)' country='USA' title='Life lessons with Katie Zaferes' cost='' />
+      <Card myID='firstCard' imgCard='./src/assets/card1.png' rate='5.0' num=' (6)' country='USA' title='Life lessons with Katie Zaferes' cost='$136' />
+      <Card imgCard='./src/assets/card2.png' rate='5.0' num=' (30)' country='USA' title='Learn edding photography' cost='$125' />
+      <Card myID='lastCard' imgCard='./src/assets/card3.png' rate='4.8' num=' (2)' country='USA' title='Group Mountain Biking' cost='$50' />
     </div>
   )
 }
