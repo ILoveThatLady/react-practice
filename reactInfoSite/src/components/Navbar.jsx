40f78acb29492cfd85d8edb9ef2f6ca0ba4b765f
Emilio Ortiz <81889373+ILoveThatLady@users.noreply.github.com>
2023-02-13 12:43:30 -0600

    Image bug fix

--- reactInfoSite/src/components/Navbar.jsx
+++ reactInfoSite/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div>
       <nav  className='nav'>
         <div>
-          <img src='src\assets\react-2.svg' className='rLogo'/>
+          <img src='https://raw.githubusercontent.com/ILoveThatLady/react-practice/cea9477c2adcba8cb930b00bece9f4d95c685808/reactInfoSite/src/assets/react-2.svg' className='rLogo'/>
           <h3>ReactFacts</h3>
         </div>
         <h4>React - Practice 1</h4>
